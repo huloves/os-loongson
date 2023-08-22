c6abe75a3561fa1371569509c33f235d61cfa204
huloves <17791627852@163.com>
2023-08-22 20:38:16 +0800

    打印hello os-loongson
    Signed-off-by: huloves <17791627852@163.com>

--- os-elephant-dev/kernel/init.c
+++ os-elephant-dev/kernel/init.c
@@ -11,9 +11,41 @@
 // #include "ide.h"
 // #include "fs.h"
 
+unsigned long uart_base = 0x1fe001e0;
+
+#define UART0_THR  (uart_base + 0)
+#define UART0_LSR  (uart_base + 5)
+#define LSR_TX_IDLE  (1 << 5)
+
+static char io_readb(unsigned long addr)
+{
+	return *(volatile char*)addr;
+}
+
+static void io_writeb(unsigned long addr, char c)
+{
+	*(char*)addr = c;
+}
+
+static void putc(char c)
+{
+	// wait for Transmit Holding Empty to be set in LSR.
+	while((io_readb(UART0_LSR) & LSR_TX_IDLE) == 0);
+	io_writeb(UART0_THR, c);
+}
+
+static void puts(char *str)
+{
+	while (*str != 0) {
+		putc(*str);
+		str++;
+	}
+}
+
 /*负责初始化所有模块 */
 void init_all()
 {
+	puts("hello os-loongson\n");
 	while(1);
 	// put_str("init_all\n");
 	// idt_init();	     // 初始化中断
