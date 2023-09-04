96e8f4bae6a99157e6099341e0da48630e9b254b
huloves <17791627852@163.com>
2023-09-05 01:16:53 +0800

    ns16550a driver

---
+++ os-elephant-dev/include/io.h
@@ -0,0 +1,139 @@
+#ifndef _IO_H
+#define _IO_H
+
+#include <stdint.h>
+
+static inline uint8_t __raw_readb(const volatile void *addr)
+{
+	return *(const volatile uint8_t *)addr;
+}
+
+static inline uint16_t __raw_readw(const volatile void *addr)
+{
+	return *(const volatile uint16_t *)addr;
+}
+
+static inline uint32_t __raw_readl(const volatile void *addr)
+{
+	return *(const volatile uint32_t *)addr;
+}
+
+static inline uint64_t __raw_readq(const volatile void *addr)
+{
+	return *(const volatile uint64_t *)addr;
+}
+
+static inline void __raw_writeb(uint8_t value, volatile void *addr)
+{
+	*(volatile uint8_t *)addr = value;
+}
+
+static inline void __raw_writew(uint16_t value, volatile void *addr)
+{
+	*(volatile uint16_t *)addr = value;
+}
+
+static inline void __raw_writel(uint32_t value, volatile void *addr)
+{
+	*(volatile uint32_t *)addr = value;
+}
+
+static inline void __raw_writeq(uint64_t value, volatile void *addr)
+{
+	*(volatile uint64_t *)addr = value;
+}
+
+#ifndef readb
+#define readb readb
+static inline uint8_t readb(const volatile void *addr)
+{
+	uint8_t val;
+
+	val = __raw_readb((void *)addr);
+	return val;
+}
+#endif
+
+#if !defined(inb) && !defined(_inb)
+#define _inb _inb
+static inline uint8_t _inb(unsigned long addr)
+{
+	uint8_t val;
+
+	val = __raw_readb((void *)addr);
+
+	return val;
+}
+#endif
+
+#if !defined(inw) && !defined(_inw)
+#define _inw _inw
+static inline uint16_t _inw(unsigned long addr)
+{
+	uint16_t val;
+
+	val = __raw_readw((void *)addr);
+	return val;
+}
+#endif
+
+#if !defined(inl) && !defined(_inl)
+#define _inl _inl
+static inline uint32_t _inl(unsigned long addr)
+{
+	uint32_t val;
+
+	val = __raw_readl((void *)addr);
+	return val;
+}
+#endif
+
+#if !defined(outb) && !defined(_outb)
+#define _outb _outb
+static inline void _outb(uint8_t value, unsigned long addr)
+{
+	__raw_writeb(value, (void *)addr);
+}
+#endif
+
+#if !defined(outw) && !defined(_outw)
+#define _outw _outw
+static inline void _outw(uint16_t value, unsigned long addr)
+{
+	__raw_writew(value, (void *)addr);
+}
+#endif
+
+#if !defined(outl) && !defined(_outl)
+#define _outl _outl
+static inline void _outl(uint32_t value, unsigned long addr)
+{
+	__raw_writel(value, (void *)addr);
+}
+#endif
+
+#ifndef inb
+#define inb _inb
+#endif
+
+#ifndef inw
+#define inw _inw
+#endif
+
+#ifndef inl
+#define inl _inl
+#endif
+
+#ifndef outb
+#define outb _outb
+#endif
+
+#ifndef outw
+#define outw _outw
+#endif
+
+#ifndef outl
+#define outl _outl
+#endif
+
+#endif /* _IO_H */
--- os-elephant-dev/kernel/init.c
+++ os-elephant-dev/kernel/init.c
@@ -10,41 +10,17 @@
 // #include "syscall-init.h"
 // #include "ide.h"
 // #include "fs.h"
-
-unsigned long uart_base = 0x1fe001e0;
-
-#define UART0_THR  (uart_base + 0)
-#define UART0_LSR  (uart_base + 5)
-#define LSR_TX_IDLE  (1 << 5)
-
-static char io_readb(unsigned long addr)
-{
-	return *(volatile char*)addr;
-}
-
-static void io_writeb(unsigned long addr, char c)
-{
-	*(char*)addr = c;
-}
-
-static void putc(char c)
-{
-	// wait for Transmit Holding Empty to be set in LSR.
-	while((io_readb(UART0_LSR) & LSR_TX_IDLE) == 0);
-	io_writeb(UART0_THR, c);
-}
+#include <ns16550a.h>
 
 static void puts(char *str)
 {
-	while (*str != 0) {
-		putc(*str);
-		str++;
-	}
+	serial_ns16550a_puts(str);
 }
 
 /*负责初始化所有模块 */
 void init_all()
 {
+	serial_ns16550a_init(9600);
 	puts("hello os-loongson\n");
 	while(1);
 	// put_str("init_all\n");
