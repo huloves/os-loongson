208d3cb3abefbe08b4cbc07872b5d747f1f03237
huloves <17791627852@163.com>
2023-09-23 10:26:59 +0800

    update

--- os-elephant-dev/loongarch/env.c
+++ os-elephant-dev/loongarch/env.c
@@ -20,6 +20,7 @@ void parse_fwargs(int a0, char **args, struct bootparamsinterface *a2)
     	int i;
 	struct _extention_list_hdr *ext_list_item = a2->extlist;
 	struct loongsonlist_mem_map *mem_info;
+	uint64_t mem_total = 0;
 
 	/**
 	 * SYSTEM_RAM  1  系统内存
@@ -28,8 +29,9 @@ void parse_fwargs(int a0, char **args, struct bootparamsinterface *a2)
 	while (ext_list_item != NULL) {
 		if (signature_cmp(ext_list_item->signature, "MEM")) {
 			mem_info = (struct loongsonlist_mem_map *) (ext_list_item);
-			for (i = 0; (i < mem_info->map_count) && (mem_info->map[i].mem_type == 1); i++) {
-				printk("mem_type: %x, mem_start: %x, mem_size: %x\n",
+			for (i = 0; (i < mem_info->map_count); i++) {
+				mem_total += mem_info->map[i].mem_size;
+				printk("mem_type: %x\tmem_start: %x\tmem_size: %x\n",
 					mem_info->map[i].mem_type, mem_info->map[i].mem_start , mem_info->map[i].mem_size);
 			}
 		}
