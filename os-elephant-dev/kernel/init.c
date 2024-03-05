4a3c61ebdff8a3d95ab83999489991b417918e9e
huloves <17791627852@163.com>
2024-03-05 23:07:52 +0800

    support 2k1000la start

--- os-elephant-dev/kernel/init.c
+++ os-elephant-dev/kernel/init.c
@@ -40,6 +40,7 @@ void init_all()
 #ifdef CONFIG_LOONGARCH64
 	serial_ns16550a_init(9600);
 	put_str("hello os-loongson\n");
+	while (1);
 #endif
 	put_str("init_all\n");
 	printk("hello %s-%c%d.%d\n", str, 'v', 0, a);
